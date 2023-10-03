77a37c830a29fb1b25bc21c0e78e4620edf263e1
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-10-03 15:41:38 +0300

    Add files via upload

---
+++ if1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace if1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblYas = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.txtYas = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblYas
+            // 
+            this.lblYas.AutoSize = true;
+            this.lblYas.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYas.Location = new System.Drawing.Point(12, 27);
+            this.lblYas.Name = "lblYas";
+            this.lblYas.Size = new System.Drawing.Size(46, 25);
+            this.lblYas.TabIndex = 0;
+            this.lblYas.Text = "yaş";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(12, 90);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(72, 25);
+            this.lblDurum.TabIndex = 1;
+            this.lblDurum.Text = "durum";
+            // 
+            // txtYas
+            // 
+            this.txtYas.BackColor = System.Drawing.Color.Silver;
+            this.txtYas.Location = new System.Drawing.Point(88, 27);
+            this.txtYas.Name = "txtYas";
+            this.txtYas.Size = new System.Drawing.Size(100, 20);
+            this.txtYas.TabIndex = 2;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(71, 139);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(134, 45);
+            this.btnHesapla.TabIndex = 3;
+            this.btnHesapla.Text = "hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(244, 196);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtYas);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblYas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblYas;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.TextBox txtYas;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ if1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+           
+                
+               
+                
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            {
+                int yas = Convert.ToInt32(txtYas.Text);
+                if (yas >= 18)
+                {
+                    MessageBox.Show("ehliyet alır");
+                }
+                else
+                {
+                    MessageBox.Show("ehliyet almaz");
+                }
+            }
+        }
+    }
+}
